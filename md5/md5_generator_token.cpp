db8ab09faad3b4b1367591017039eddf1afae3d7
jack <jack@is-land.com.tw>
2017-05-09 01:56:19 -0400

    Modify md5 geerator code

--- md5/md5_generator_token.cpp
+++ md5/md5_generator_token.cpp
@@ -35,27 +35,3 @@ private:
     string _word;
 
 };
-
-
-/*GeneratorMD5::GeneratorMD5(const string &word){
-  _word = word;
-}
-
-GeneratorMD5::~GeneratorMD5(){
-
-}
-
-string GeneratorMD5::render(){
-  unsigned char digest[MD5_DIGEST_LENGTH];
-  char wordArray[sizeof(_word)];
-  strcpy(wordArray, _word.c_str());
-
-  MD5((unsigned char*)&wordArray, strlen(wordArray), (unsigned char*)&digest);
-  char mdString[33];
-  int i;
-  for(i = 0; i < 16; i++){
-     sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
-  }
-
-   return mdString;
-}*/
